7d83d4396ce7c5bb80af53225f65465a255b8590
Szymon <osx@osxs-iMac.local>
2024-12-03 23:58:57 +0100

    test3

--- src/DailyChallanges/test.java
+++ src/DailyChallanges/test.java
@@ -2,6 +2,6 @@ package DailyChallanges;
 
 public class test  {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test github");
     }
 }
